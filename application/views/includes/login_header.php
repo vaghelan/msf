9d8bba8f5cd40a7c1ea08a2752913ea6f5e755f3
Nilesh Vaghela <vaghelan@gmail.com>
2011-05-27 14:23:57 -0600

    o IE 9 update

--- application/views/includes/login_header.php
+++ application/views/includes/login_header.php
@@ -8,18 +8,35 @@
         <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/style_ie.css" />
 <![endif]-->
 
-<!--[if lt IE 8]>
-<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE8.js"></script>
-<![endif]-->
-
-<!--[if lt IE 9]>
-<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
-<![endif]-->
  <!--[if IE]>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/style_ie.css" />
 <![endif]-->
 
-       
+<!--[if gte IE 9]>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/style_ie9.css" />
+<![endif]-->
+
+              
 </head>
 <body>
+
+<div id="ie" style="display: none; background: url(<?php echo base_url();?>css/br/bg_t.png) repeat;">
+<div class="aler">
+<h1>Don't get us wrong, we did NOT block IE9, it just didn't work.</h1>
+
+<h1>Sankirtan Book Distribution Management System only works with other browsers.
+<p>Supported browser, you can download:</p>
+<div class="content">
+  <p>
+  	<a title="safari" target="_self" href="http://www.apple.com/safari/" class="fl browser-logo"><img height="64" width="64" alt="safari" src="<?php echo base_url();?>css/br/safari.png"></a>
+  	<a title="firefox" target="_self" href="http://www.mozilla.com/firefox/" class="fl browser-logo"><img height="64" width="64" alt="firefox" src="<?php echo base_url();?>css/br/firefox.png"></a>
+  	<a title="chrome" target="_self" href="http://www.google.com/chrome" class="fl browser-logo"><img height="64" width="64" alt="opera" src="<?php echo base_url();?>css/br/chrome.png"></a>
+	<a title="opera" target="_self" href="http://www.opera.com/browser/" class="fl browser-logo"><img height="64" width="64" alt="opera" src="<?php echo base_url();?>css/br/opera.png"></a>
+  	<a title="camino" target="_self" href="http://caminobrowser.org/" class="fl browser-logo"><img height="64" width="64" alt="camino" src="<?php echo base_url();?>css/br/camino.png"></a>
+  	<a title="seamonkey" target="_self" href="http://www.seamonkey-project.org/" class="fl browser-logo"><img height="64" width="64" alt="seamonkey" src="<?php echo base_url();?>css/br/seamonkey.png"></a>
+  </p>
+</div>
+</div>
+
+</div>
 
