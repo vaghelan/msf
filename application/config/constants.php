cb6bf6daa3af18854f316efbd8a3927a449b42f2
Nilesh Vaghela <vaghelan@gmail.com>
2011-06-07 16:27:52 -0600

    o bump version to 4.1.2

--- application/config/constants.php
+++ application/config/constants.php
@@ -37,6 +37,6 @@ define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 
-define('APP_VERSION', '4.1.1');
+define('APP_VERSION', '4.1.2');
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
