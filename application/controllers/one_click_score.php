511f93cfd55c5f00aed0801294eac76e2864d014
Nilesh Vaghela <vaghelan@gmail.com>
2011-06-19 23:57:04 -0600

    o final one click report functionality

--- application/controllers/one_click_score.php
+++ application/controllers/one_click_score.php
@@ -15,12 +15,42 @@ class One_Click_Score extends CI_Controller
       echo "Invalid cookie";
       return;
     }  
+    $user_info = $this->membership_model->get_user_details_by_id($userid);
+    $data['url'] = $userid . "/" . $cookie . "/" . $count;
+    
+    $data['name'] = $user_info->name;
+    
+   // echo "Username = " .  $user_info->name;
+    
+    $this->load->view('one_click_report_init', $data);
+    
+              
+    
+  
+  }
+
+  function report_confirm($userid, $cookie, $count)
+  {
+    if (false == $this->membership_model->validate_user_id($userid))
+    {
+      echo "Invalid userid";
+      return;
+    } 
+    
+    if (false == $this->users_data_model->validate_cookie($userid, $cookie))
+    {
+      echo "Invalid cookie";
+      return;
+    }  
     $cid = $this->events_model->get_current_event_id();
     $tday = date('Y-m-d');
     $bid = $this->books_model->get_book_id('Other');
-    
-   // $this->add_score($cid, $userid, $bid, $this->input->post('count'), $tday)); 
-    $data['num'] = $this->scores_model->get_total_books_distributed_by_user_and_eventid($userid, $cid);
+     
+    $data['num'] = $result = $this->scores_model->get_total_books_distributed_by_user_and_eventid($userid, $cid);
+    if ($result == 0)
+    {
+      $this->scores_model->add_score($cid, $userid, $bid, $count, $tday);
+    }
     $this->load->view('one_click_report_view', $data);
   }
   
--- application/views/one_click_report_view.php
+++ application/views/one_click_report_view.php
@@ -1,15 +1,38 @@
 <html>
+
+<head>
+       <title>Scores Report!</title>
+        <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="screen" />
+<style>
+a {
+color: #00aeff;
+}
+.form_{
+width: 400px !important;
+}
+.form_ h1{
+	margin-top: 0px;
+}
+</style>
+
+</head>
+
 <body>
+<div id="one_click_report_view" class="form_">
+<div class="img_sign"></div>
 <?php
 if ($num == 0)
 {
-  echo "Congrats!! You are now counted. Thank you very much";
+  echo "<h1>Congrats!! You are now counted.</h1> <p>Thank you very much.</p>";
 }
 else
-  echo "You are already counted."; 
+
+  echo "<h1>You are already counted.</h1>"; 
 
 echo "If you want to report more books, please <a href=\"http://7thgoswami.com/account/?login\">login</a> and add books.";
 ?> 
+
+</div>
 </body>
 
 </html>
